9f9a77927d3af1958fd70d967e55a8e874f5bb57
recursive-certificate-9133 <wjhuang0726@gmail.com>
2018-12-11 04:43:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def line(deli_line)
+  line_array=[]
+  if deli_line.length==0
+    puts "The line is currently empty."
+  else
+    deli_line.each.each_with_index do |name, index|
+      line_array.push(" #{index+1}. #{name}")
+    end
+    
+    puts "The line is currently:"+line_array.join()
+  end
+  
+  
+end
+
+def take_a_number(deli_line, name)
+  deli_line.push(name)
+  puts "Welcome, #{name}. You are number #{deli_line.length} in line."
+end
+
+def now_serving(deli_line)
+  if deli_line.length == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli_line.shift}."
+  end
+end
+
