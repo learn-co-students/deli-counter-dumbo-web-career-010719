5dcc63a686fd8eff68cdf78f859bdf344ff90462
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-14 02:16:30 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def line(arr)
+  if arr.size == 0
+    puts "The line is currently empty."
+    return
+  else
+    output = "The line is currently: "
+    arr.each_with_index do |name, index|
+      output += "#{index + 1}. #{name} "
+    end
+  end
+  puts output.rstrip
+end
+
+def take_a_number(arr, name)
+  arr.push(name)
+  puts "Welcome, #{name}. You are number #{arr.index(name) + 1} in line."
+end
+
+def now_serving(arr)
+  if arr.size == 0
+    puts "There is nobody waiting to be served!"
+    return
+  end
+  puts "Currently serving #{arr.shift}."
+end
